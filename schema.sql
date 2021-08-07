b79d676ea84ace94216210817f842d566a4b3c98
Tannerls <83738699+Tannerls@users.noreply.github.com>
2021-08-07 15:44:09 -0500

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -52,8 +52,7 @@ CREATE TABLE department_employee (
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (emp_no),
-	UNIQUE (dept_no)
+	PRIMARY KEY (emp_no, dept_no)
 );
 
 SELECT * FROM departments;
